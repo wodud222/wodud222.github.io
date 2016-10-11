27a815a38f9d160cb30197d6ad6e4b810193e3bf
Lee Jaeyoung <wodud222@gmail.com>
2016-10-11 19:22:43 +0900

    portfolio pptx / mainpage 일부 코딩

---
+++ portfolio/main_page/js/jquery/mainpage.js
@@ -0,0 +1,18 @@
+/*mainpage.js*/
+
+
+(function($){
+
+	$('#menuBtn').on('mouseenter',function(){
+
+	var make = $('#wrap>div:nth-of-type(2)');
+
+	make.toggleClass('showBox');
+
+
+
+
+    });
+
+
+})(this.jQuery)
